50a279d4f0e26c306d4e151377f58cab1309120d
neaimt <pas120344@gmail.com>
2024-01-12 22:27:45 +0900

    1929 cpp

---
+++ BOJ/1929.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[1000001];
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    for (int i = 2; i <= n; i++)
+        arr[i] = i;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (arr[i] == 0) continue;
+
+        for (int j = i * i; j <= n; j += i)
+            arr[j] = 0;
+    }
+
+    for (int i = m; i <= n; i++) 
+        if (arr[i] != 0)
+            cout << arr[i] << '\n';
+}
